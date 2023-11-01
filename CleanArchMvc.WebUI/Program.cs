ab5f541dc7f276c39b2f39b98561dc29152c8cb0
Kenzo Mateus <kenzomateus44@gmail.com>
2023-11-01 14:51:27 -0300

    Adding dependencies between projects

--- CleanArchMvc.WebUI/Program.cs
+++ CleanArchMvc.WebUI/Program.cs
@@ -1,3 +1,11 @@
+// RESPONSABILIDADES DE CADA CAMADA;
+
+// CleanArch.Domain: Modelo de dominio, regras de negocio, interfaces; (Não possui nenbuma dependencia);
+// CleanArch.Application: Regras de dominio da aplicação, Mapeamentos, serviços, DTOs, CQRS; (Dependencia com o Projeto Domain);
+// CleanArch.Infra.Data: EF Core, Contexto, Configurações, Migrations, Repository; (Dependencia com o Projeto Domain);
+// CleanArch.Infra.Ioc: Dependncy Injection, Registro dos serviços, tempo de vida; (Dependencia com os projetos: Domain, Application, Infra.Data) 
+// CleanArch.WebUI: MVC, Controllers, Views, Filtros, ViewModels(MVC); (Dependencia com o Infra.Ioc);
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
