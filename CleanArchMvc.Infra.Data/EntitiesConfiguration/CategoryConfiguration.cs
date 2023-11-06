a989dc91a358105f95e768d1e64a2ae6e016324b
Kenzo Mateus <kenzomateus44@gmail.com>
2023-11-06 18:11:58 -0300

    configuring entities

---
+++ CleanArchMvc.Infra.Data/EntitiesConfiguration/CategoryConfiguration.cs
@@ -0,0 +1,31 @@
+﻿using CleanArchMvc.Domain.Entitites;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// configurando as entidades para mapeamento no banco de dados
+namespace CleanArchMvc.Infra.Data.EntitiesConfiguration
+{
+    public class CategoryConfiguration : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            // reforçando que a propriedade Id vai ser a chave primaria
+            builder.HasKey(c => c.Id);
+
+            // configurando a propriedade name com no maximo 100 caracteres é o tornando obrigatorio não aceitando valores nulos
+            builder.Property(c => c.Name).HasMaxLength(100).IsRequired();
+
+            // Populando o banco de dados
+            builder.HasData(
+                new Category(1, "Material Escolar"),
+                new Category(2, "Eletrônicos"),
+                new Category(3, "Acessórios")
+            );
+        }
+    }
+}
---
+++ CleanArchMvc.Infra.Data/EntitiesConfiguration/ProductConfiguration.cs
@@ -0,0 +1,32 @@
+﻿using CleanArchMvc.Domain.Entitites;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.Infra.Data.EntitiesConfiguration
+{
+    public class ProductConfiguration : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            // reforçando que a propriedade Id vai ser a chave prim
+            builder.HasKey(p => p.Id);
+
+            // configurando a propriedade name com no maximo 100 caracteres é o tornando obrigatorio não aceitando valores nulos
+            builder.Property(p => p.Name).HasMaxLength(100).IsRequired();
+
+            // configurando a propriedade description com no maximo 200 caracteres é o tornando obrigatorio não aceitando valores nulos
+            builder.Property(p => p.Description).HasMaxLength(200).IsRequired();
+
+            // configurando a propriedade Price com uma precisão de 10 posições e 2 casas decimais.
+            builder.Property(p => p.Price).HasPrecision(10,2).IsRequired();
+
+            // configurando o relacionamento de 1 para muitos entre categoria e produtos e reforçando que CategoryId é a chave estrangeira
+            builder.HasOne(e => e.Category).WithMany(e => e.Products).HasForeignKey(e => e.CategoryId);
+        }
+    }
+}
